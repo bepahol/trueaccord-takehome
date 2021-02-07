baa79c3e5d0407ccf3a8e10686ca5affe387acc1
Benjamin Holst <bepahol@gmail.com>
2021-02-07 13:16:10 -0700

    DebtInfo: forgot to add remainingAmount

--- src/trueaccord/DebtInfo.java
+++ src/trueaccord/DebtInfo.java
@@ -4,17 +4,19 @@ public class DebtInfo {
     
     private Debt debt;
     private boolean isInPaymentPlan;
+    private double remainingAmount;
     private String nextPaymentDueDate;
     
-    public DebtInfo(Debt debt, boolean isInPaymentPlan, String nextPaymentDueDate) {
+    public DebtInfo(Debt debt, boolean isInPaymentPlan, double remainingAmount, String nextPaymentDueDate) {
         this.debt = debt;
         this.isInPaymentPlan = isInPaymentPlan;
+        this.remainingAmount = remainingAmount;
         this.nextPaymentDueDate = nextPaymentDueDate;
     }
 
     @Override
     public int hashCode() {
-        return debt.hashCode() ^ Boolean.hashCode(isInPaymentPlan) ^ nextPaymentDueDate.hashCode();
+        return debt.hashCode() ^ Boolean.hashCode(isInPaymentPlan) ^ Double.hashCode(remainingAmount) ^ nextPaymentDueDate.hashCode();
     }
     
     @Override
@@ -30,6 +32,7 @@ public class DebtInfo {
         DebtInfo other = (DebtInfo) o;
         return this.debt == other.debt && 
                 this.isInPaymentPlan == other.isInPaymentPlan &&
+                this.remainingAmount == other.remainingAmount &&
                 this.nextPaymentDueDate.equals(other.nextPaymentDueDate);
     }
 }
--- test/trueaccord/DebtInfoTest.java
+++ test/trueaccord/DebtInfoTest.java
@@ -31,9 +31,9 @@ public class DebtInfoTest {
     @Test
     public void testEquals() {
         Debt debt = new Debt(0, 123.46);
-        DebtInfo debtInfo1 = new DebtInfo(debt, true,  "abc");
-        DebtInfo debtInfo2 = new DebtInfo(debt, true,  "");
-        DebtInfo debtInfo3 = new DebtInfo(debt, false, "abc");
+        DebtInfo debtInfo1 = new DebtInfo(debt, true,  100.2, "abc");
+        DebtInfo debtInfo2 = new DebtInfo(debt, true,  100.2, "");
+        DebtInfo debtInfo3 = new DebtInfo(debt, false, 100.2, "abc");
         
         assertEquals(debtInfo1, debtInfo1);
         assertNotEquals(debtInfo1, debtInfo2);
