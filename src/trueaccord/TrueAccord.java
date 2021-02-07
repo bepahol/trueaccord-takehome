c246867c71804fc3a4a06c12a49e1149949836e1
Benjamin Holst <bepahol@gmail.com>
2021-02-06 17:06:48 -0700

    init project

---
+++ src/trueaccord/TrueAccord.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trueaccord;
+
+/**
+ *
+ * @author ben-pc
+ */
+public class TrueAccord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
